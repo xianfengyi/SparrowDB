4cd937b5f3952af093f8b326250a1d64b7851652
yixianfeng <yixianfeng@bytedance.com>
2022-11-01 13:29:01 +0800

    完善代码格式

--- sparrow-cli/src/main/java/com/pioneer/sparrowdb/cli/Sparrow.java
+++ sparrow-cli/src/main/java/com/pioneer/sparrowdb/cli/Sparrow.java
@@ -2,8 +2,7 @@ package com.pioneer.sparrowdb.cli;
 
 public class Sparrow {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Console console = new Console();
         System.exit(console.run() ? 0 : 1);
     }
